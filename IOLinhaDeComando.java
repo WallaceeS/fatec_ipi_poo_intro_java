eb898e9c7ae7fc9322ccb7a3f7fbdc7998569adc
Wallace Souza <wallacesilvadesouzaa@outlook.com>
2025-08-28 15:53:25 -0300

    learn(ap002-java): encerra apostila

---
+++ IOLinhaDeComando.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class IOLinhaDeComando{
+    public static void main(String [] args){
+        //declaração de variáveis
+        int a, b;
+        int resultado;
+        Scanner leitor = new Scanner(System.in);
+        //entrada
+        System.out.println("Digite o primeiro valor");
+        a = leitor.nextInt();
+        System.out.println("Digite o segundo valor");
+        b = leitor.nextInt();
+        //processamento
+        resultado = a + b;
+        //saída
+        System.out.printf("%d + %d = %d\n", a, b, resultado);
+    }
+}
